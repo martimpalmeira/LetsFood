c8a4a271365f10ae0bbdfea729f67c65e65b5296
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-09-12 01:22:31 -0300

    Missing animations and carousel

--- assets/js/controllers/mobile-menu-controller.js
+++ assets/js/controllers/mobile-menu-controller.js
@@ -1,10 +1,19 @@
 const menuMobile = document.querySelector('.mobile-nav');
-const menuIcon = document.querySelector('.mobile-icon-container')
+const menuIcon = document.querySelector('.mobile-icon-container');
+const spanMId = document.querySelector('#hamburguer-mid');
+const spanBot = document.querySelector('#hamburguer-bot');
+const spanTop = document.querySelector('#hamburguer-top');
+
+
 
 export function init(){
     
     menuIcon.addEventListener('click', ()=>{
         
         menuMobile.classList.toggle('show-mobile-nav');
+        
+        spanMId.classList.toggle('changeSpanMid');
+        spanBot.classList.toggle('changeSpanBot');
+        spanTop.classList.toggle('changeSpanTop');
     })
 }
